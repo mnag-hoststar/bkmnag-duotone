9d0f67c2129c056d757ed6e691ebcc104b6f3127
Eva Jutzeler <eju@hoststar.ch>
2018-05-24 14:47:47 +0200

    Fixing errors with header overlap over the content.

--- js/navigation.js
+++ js/navigation.js
@@ -43,62 +43,29 @@ $(document).ready(function() {
 	function languageSelector() {
     	if ($('.language-selector')) {
     	    var languageSelectorHeight = $('.language-selector').outerHeight();
+    	    var navigationHeight = $('.template__header').outerHeight();
+    	    var footerHeight = $('.template__footer').outerHeight();
     	    
-			$('section.page').css('margin-top', languageSelectorHeight+'px');
+    	    if (languageSelectorHeight==undefined) {
+    	        var languageAndNavigationHeight = navigationHeight;
+    	    } else {
+    	        var languageAndNavigationHeight = languageSelectorHeight+navigationHeight;
+    	    }
+    	    
 			$('.template__header').css('margin-top', languageSelectorHeight+'px');
-			$('.header-feature').css('padding-bottom', languageSelectorHeight+'px');
-			$('.feature__background-image').css('min-height', 'calc(95vh - '+languageSelectorHeight+'px)');
+			$('.template__content').css('margin-top', languageAndNavigationHeight+'px');
+			$('.page').css('margin-bottom', footerHeight+'px');
     	}
 	}
 	
 	languageSelector();
-
-	// ------------------------------
-	// Change the top padding of the content depending on how high
-	// the navigation header and footer is.
-	// ------------------------------
-
-	var headerHeight;
-	var footerHeight;
-
-	function resizeHeaderFooter() {
-		if ($(window).width() < 720) {
-			headerHeight = $('.template__header').outerHeight()+20;
-			footerHeight = $('.template__footer').outerHeight()+30;
-		} else {
-			headerHeight = $('.template__header').outerHeight()+30;
-			footerHeight = $('.template__footer').outerHeight()+60;
-		}
-
-		return headerHeight, footerHeight;
-	}
 	
-	function resizeContent() {
-		$('.template__content').css('padding-top', headerHeight +'px');
-		$('.default section.page').css('padding-bottom', footerHeight +'px');
-	}
-	
-	resizeHeaderFooter();
-	resizeContent();
-
-	$(window).resize(function() {
-		resizeHeaderFooter();
-		resizeContent();
-	});
-	
-	// ------------------------------
-	// Feature Overlay Corrections
-	// The overlay of the feature should be below the header.
-	// ------------------------------
-	
-	function resizeHeaderHeight() {
-    	var headerHeight = $('.template__header').outerHeight();
-    	var featureOverlayTopDistance = $('.widget[data-widget-type="feature"]').attr('style', 'top: ' + headerHeight + 'px');
-	}
-
-	$(window).resize(function() {
-		resizeHeaderHeight();
-	});
+    $(document).bind("DOMSubtreeModified",function(){
+        languageSelector();
+		$(window).resize(function() {
+			languageSelector();
+		});
+    });
     
 	// ------------------------------
     // Hides other submenus when a main menu point with a submenu is clicked.
@@ -106,14 +73,5 @@ $(document).ready(function() {
     
     $(".navigation-item").click(function(){
         $(this).siblings(".navigation-item").removeClass("folder-open open");
-    });
-
-	// ------------------------------
-    // Changes by the editor are saved in the style elmement with the class js-compiled-styles.
-	// ------------------------------
-	
-    $(document).bind("DOMSubtreeModified",function(){
-        languageSelector();
-		resizeHeaderHeight();
     });
 });
