e2f139705e43dab6164e239055df31ad1b49c8df
Eva Jutzeler <eju@hoststar.ch>
2019-01-16 11:56:17 +0100

    Fixing parallax effect, because it looked odd on Firefox on Windows. OTRS Ticket#2019011542001785

--- js/header-feature.js
+++ js/header-feature.js
@@ -4,46 +4,15 @@
 
 // Feature parallax fade effect.
 
-var featureHeight = $(".header-feature").height();
-var featurePosition = $(".header-feature").position();
-var featureTriggerValue = featureHeight + featurePosition.top;
-var window_height = window.innerHeight;
-var offsetAmount;
-
-function checkFeatureDimensions() {
-    featureHeight = $(".header-feature").height();
-    featurePosition = $(".header-feature").position();
-    featureTriggerValue = featureHeight + featurePosition.top;
-    window_height = window.innerHeight;
+function parallaxFade() {
+	scrollPos = $(this).scrollTop();
+	$('.feature__content-wrap').css({
+		'margin-bottom': (-scrollPos/4)+"px",
+		'opacity': 1-(scrollPos/500)
+	});
 }
-var scrollEffect = function (elem) {
-
-	// How high the window is
-
-	if ( window.pageYOffset > (featureTriggerValue - window_height)  ) {
-
-		if(featureTriggerValue > window_height) {
-			offsetAmount = window.pageYOffset - (featureTriggerValue - window_height);
-		}
-		else {
-			offsetAmount = window.pageYOffset/4;
-		}
-
-		// Apply styles as user scrolls
-		elem.setAttribute(
-			"style", "opacity:" + Math.max(0, parseFloat(1-(2/window_height*offsetAmount)).toFixed(2)) + "; -webkit-transform: translateY(" + offsetAmount + "px)"
-		);
-	}
-};
-
-var elem = document.querySelector('.content-inner-wrap');
-
-window.onscroll = function(e) {
-    if (elem) {
-        scrollEffect(elem);
-    }
-};
-
-$( window ).resize(function() {
-  checkFeatureDimensions();
+$(document).ready(function(){
+	$(window).scroll(function() {
+		parallaxFade();
+	});
 });
