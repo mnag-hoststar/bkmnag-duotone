621e375c88bd12843c848fdcf882e0d3d6540eb6
Eva Jutzeler <eju@hoststar.ch>
2019-10-22 11:13:23 +0200

    Fix bug where parallax effect for the image has disappeared.

--- js/header-feature.js
+++ js/header-feature.js
@@ -6,6 +6,9 @@
 
 function parallaxFade() {
 	scrollPos = $(this).scrollTop();
+	$('.feature__background-image').css({
+		'top': (scrollPos/2)+"px"
+	});
 	$('.feature__content-wrap').css({
 		'margin-bottom': (-scrollPos/4)+"px",
 		'opacity': 1-(scrollPos/500)
